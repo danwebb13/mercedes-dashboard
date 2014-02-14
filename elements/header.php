a8face64df382799bef3e3ebcd07defe4edfd985
Dan Webb <dan@dan-webb.com>
2014-02-14 12:01:39 +0000

    Built the header and nav

--- elements/header.php
+++ elements/header.php
@@ -1,21 +1,22 @@
 <header>
     <div class="container">
         <a class="logo" href="/">
-            <img src="/assets/images/logos/logo-mercedes-benz.png" alt="Mercedes Benz" />
+            <img src="/assets/images/logos/logo-mercedes-benz.gif" alt="Mercedes Benz" />
         </a>
-        <h1>Corporate Drive Safety Training</h1>
+        <h1>
+            <img class="slogan" src="/assets/images/logos/slogan.gif" alt="Corporate Drive Safety Training" />
+        </h1>
     </div>
 
-    <div class="nav-container">
-        <div class="container">
-            <nav>
-                <ul>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                </ul>
-            </nav>
-        </div>
+    <div class="container">
+        <nav class="nav-primary">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">My Settings</a></li>
+                <li><a href="">Help</a></li>
+                <li><a href="">Logout</a></li>
+            </ul>
+            <a class="nav-link" href="#">PDF Downloads</a>
+        </nav>
     </div>
 </header>
