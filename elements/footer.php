6f38dab4c09e9ff1b4d3580d825ed8de317a4de7
Dan Webb <dan@dan-webb.com>
2014-02-15 21:51:57 +0000

    Created the footer, added the pdf link to the bottom of the page

--- elements/footer.php
+++ elements/footer.php
@@ -1,3 +1,6 @@
-<footer>
-	&nbsp;
-</footer>
+<ul class="footer-items">
+	<li class="footer-item"><p class="footer-item-text">&copy; 2004-2014 Mercedes-Benz UK Ltd</p></li>
+	<li class="footer-item"><a class="footer-item-text" href="">News</a></li>
+	<li class="footer-item"><a class="footer-item-text" href="">Privacy Policy</a></li>
+	<li class="footer-item"><a class="footer-item-text" href="">Legal Notice</a></li>
+</ul>
--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <div class="container">
         <div class="page">
 
-            <section class="page--header">
+            <section>
                 <div class="intro">
                     <?php include 'elements/home/intro.php'; ?>
                 </div>
@@ -24,15 +24,20 @@
                 </div>
             </section>
 
-            <section class="data-table">
+            <section>
                 <?php include 'elements/modules/table-profile.php' ?>
+                <a class="link-small" href="#">Download a pdf of this page</a>
             </section>
 
         </div>
     </div>
 
     <!-- Footer -->
-    <?php include 'elements/footer.php'; ?>
+    <div class="container">
+        <footer>
+            <?php include 'elements/footer.php'; ?>
+        </footer>
+    </div>
 
     <!-- Javascript -->
     <?php include 'elements/js.php'; ?>
