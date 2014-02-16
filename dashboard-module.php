becde25868eb948c197883c8ddb5fb4767bb9652
Dan Webb <dan@dan-webb.com>
2014-02-16 22:43:07 +0000

    Created the notes section

--- dashboard-module.php
+++ dashboard-module.php
@@ -22,7 +22,7 @@
                 </div>
             </section>
 
-            <section>
+            <section class="section--spaced">
                 <div class="l-col3">
                     <div class="l-col3-col l-col3-col1">
                         <div class="js-dashboard-module dashboard-module">
@@ -52,6 +52,15 @@
                         </div>
                     </div>
                 </div>
+            </section>
+
+            <section>
+                <h4 class="notes--heading">Proactive Driving Module Notes</h4>
+                <ul class="notes">
+                    <li>Vivamus vestibulum auctor elit ut volutpat. Vestibulum ut mauris in justo blandit feugiat quis ac nibh. In auctor ligula enim, vel volutpat arcu gravida sed. Integer sem arcu, placerat vitae nisi id, rutrum luctus turpis. Curabitur accumsan nibh id magna elementum, ac bibendum tortor interdum.</li>
+                    <li>Etiam mollis et lacus et eleifend. Morbi ut diam vel ipsum euismod elementum. In facilisis augue magna, at venenatis velit imperdiet at. Morbi iaculis, eros quis aliquet ornare, leo felis mattis dui, at consequat risus ligula in mauris.</li>
+                    <li>Sed aliquet metus luctus iaculis ullamcorper. Ut sed vehicula erat. Proin semper nibh sit amet odio eleifend posuere. Proin suscipit hendrerit velit, in laoreet erat cursus quis.</li>
+                </ul>
             </section>
 
         </div>
