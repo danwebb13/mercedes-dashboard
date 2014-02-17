2d87f17b9b8368871c59b14717cd8ee75489d293
Dan Webb <dan@dan-webb.com>
2014-02-17 23:22:25 +0000

    Added the html5 shiv to the head

--- elements/parts/head.php
+++ elements/parts/head.php
@@ -4,4 +4,7 @@
     <link href='http://fonts.googleapis.com/css?family=Comfortaa:400,700,300' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="assets/css/all.css" />
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+	<!--[if lt IE 9]>
+		<script src="assets/js/libs/html5shiv.js"></script>
+	<![endif]-->
 </head>
