4c93e7f8f9fd627bfa7087b04c245fce3055ef38
Dan Webb <dan@dan-webb.com>
2014-02-26 14:33:02 +0000

    Changed the width of the notes and font sizes of dashboard module elements

--- dashboard-module.php
+++ dashboard-module.php
@@ -66,12 +66,14 @@
                 </section>
 
                 <section>
-                    <h4 class="notes--heading">Proactive Driving Module Notes</h4>
-                    <ul class="notes">
-                        <li>Vivamus vestibulum auctor elit ut volutpat. Vestibulum ut mauris in justo blandit feugiat quis ac nibh. In auctor ligula enim, vel volutpat arcu gravida sed. Integer sem arcu, placerat vitae nisi id, rutrum luctus turpis. Curabitur accumsan nibh id magna elementum, ac bibendum tortor interdum.</li>
-                        <li>Etiam mollis et lacus et eleifend. Morbi ut diam vel ipsum euismod elementum. In facilisis augue magna, at venenatis velit imperdiet at. Morbi iaculis, eros quis aliquet ornare, leo felis mattis dui, at consequat risus ligula in mauris.</li>
-                        <li>Sed aliquet metus luctus iaculis ullamcorper. Ut sed vehicula erat. Proin semper nibh sit amet odio eleifend posuere. Proin suscipit hendrerit velit, in laoreet erat cursus quis.</li>
-                    </ul>
+                    <div class="notes">
+                        <h4 class="notes--heading">Proactive Driving Module Notes</h4>
+                        <ul class="notes--list">
+                            <li>Vivamus vestibulum auctor elit ut volutpat. Vestibulum ut mauris in justo blandit feugiat quis ac nibh. In auctor ligula enim, vel volutpat arcu gravida sed. Integer sem arcu, placerat vitae nisi id, rutrum luctus turpis. Curabitur accumsan nibh id magna elementum, ac bibendum tortor interdum.</li>
+                            <li>Etiam mollis et lacus et eleifend. Morbi ut diam vel ipsum euismod elementum. In facilisis augue magna, at venenatis velit imperdiet at. Morbi iaculis, eros quis aliquet ornare, leo felis mattis dui, at consequat risus ligula in mauris.</li>
+                            <li>Sed aliquet metus luctus iaculis ullamcorper. Ut sed vehicula erat. Proin semper nibh sit amet odio eleifend posuere. Proin suscipit hendrerit velit, in laoreet erat cursus quis.</li>
+                        </ul>
+                    </div>
                 </section>
 
             </div>
