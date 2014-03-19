d5383e754a213293e5e7f3a903de8a2a2f559c2b
Dan Webb <dan@dan-webb.com>
2014-03-19 01:17:22 +0000

    Added the custom css link tag to head

--- elements/parts/head.php
+++ elements/parts/head.php
@@ -4,6 +4,8 @@
     <title>Mercedes-Benz Driver Profile</title>
     <meta name="description" content="" />
     <link rel="stylesheet" href="/assets/css/all.css" />
+    <link rel="stylesheet" href="/assets/css/custom.css" />
+
     <link href="/assets/css/custom.css" rel="stylesheet" type="text/css" />
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
 	<!--[if lt IE 9]>
